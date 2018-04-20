e1d6d5760506745791d7ddfae879226e82f79a4d
bwyfed <36172659@qq.com>
2018-04-20 18:04:36 +0800

    设计项目架构的路由配置，解决扩展名检查报错的问题。

--- client/app.js
+++ client/app.js
@@ -3,8 +3,9 @@
  */
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { AppContainer } from 'react-hot-loader' //eslint-disable-line
-import App from './views/App.jsx'
+import App from './views/App'
 
 // ReactDOM.hydrate(<App />, document.getElementById('root'));
 const root = document.getElementById('root');
@@ -12,15 +13,17 @@ const render = (Component) => {
   const renderMethod = module.hot ? ReactDOM.render : ReactDOM.hydrate
   renderMethod(
     <AppContainer>
-      <Component />
+      <BrowserRouter>
+        <Component />
+      </BrowserRouter>
     </AppContainer>,
     root,
   )
 };
 render(App);
 if (module.hot) {
-  module.hot.accept('./views/App.jsx', () => {
-		const NextApp = require('./views/App.jsx').default; //eslint-disable-line
+  module.hot.accept('./views/App', () => {
+		const NextApp = require('./views/App').default; //eslint-disable-line
     // ReactDOM.hydrate(<NextApp />, document.getElementById('root'));
     render(NextApp);
   })
--- client/server-entry.js
+++ client/server-entry.js
@@ -2,6 +2,6 @@
  * Created by BWY on 2018/4/18.
  */
 import React from 'react'
-import App from './views/App.jsx'
+import App from './views/App'
 
 export default <App />
