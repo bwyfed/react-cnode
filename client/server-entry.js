98cfbfa093b5b9265c9a5ced58aa2dfeea4b7d03
bwyfed <36172659@qq.com>
2018-04-20 15:57:21 +0800

    设计项目架构的基本目录结构。

--- client/server-entry.js
+++ client/server-entry.js
@@ -2,6 +2,6 @@
  * Created by BWY on 2018/4/18.
  */
 import React from 'react'
-import App from './App.jsx'
+import App from './views/App.jsx'
 
 export default <App />
---
+++ client/views/topic-detail/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class TopicDetail extends React.Component {
+  componentDidMount() {
+    // do somethin here
+  }
+
+  render() {
+    return (
+      <div>This is topic detail</div>
+    )
+  }
+}
