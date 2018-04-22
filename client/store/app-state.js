6c7e8e56dbc0400a6d5e19b83941435566a8fef3
bwyfed <36172659@qq.com>
2018-04-22 16:45:24 +0800

    通过Mobx机制实现状态管理。

---
+++ client/store/app-state.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Hello on 2018/4/22.
+ */
+import { observable, computed, autorun, action } from 'mobx'
+
+class AppState {
+  @observable count = 0
+  @observable name = 'Jokcy'
+  @computed get msg() {
+    return `${this.name} say count is ${this.count}`
+  }
+  @action add() {
+    this.count += 1
+  }
+  @action changeName(name) {
+    this.name = name
+  }
+}
+
+const appState = new AppState()
+// 测试用代码
+autorun(() => {
+  console.log(appState.msg)
+})
+
+setInterval(() => {
+  appState.add()
+}, 1000)
+
+export default appState
