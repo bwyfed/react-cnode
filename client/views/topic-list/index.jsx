7fa1c559b1899a62fbfd2b0676c10b68c14e6185
bwyfed <36172659@qq.com>
2018-04-27 11:51:48 +0800

    实现主页面的样式，实现列表项的结构和样式。

--- client/views/topic-list/index.jsx
+++ client/views/topic-list/index.jsx
@@ -2,15 +2,23 @@ import React from 'react'
 import { observer, inject } from 'mobx-react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+
+import Tabs, { Tab } from 'material-ui/Tabs'
 import Button from 'material-ui/Button'
 import { AppState } from '../../store/app-state'
 import Container from '../layout/container'
+import TopicListItem from './list-item'
 @inject('appState') @observer
 
 export default class TopicList extends React.Component {
   constructor() {
     super()
-    this.changeName = this.changeName.bind(this)
+    this.state = {
+      tabIndex: 0,
+    }
+    this.changeName = this.changeName.bind(this) // 测试用代码
+    this.changeTab = this.changeTab.bind(this)
+    this.listItemClick = this.listItemClick.bind(this)
   }
   componentDidMount() {
     // do somethin here
@@ -28,14 +36,43 @@ export default class TopicList extends React.Component {
   changeName(event) {
     this.props.appState.changeName(event.target.value)
   }
+  changeTab(e, index) {
+    this.setState({
+      tabIndex: index,
+    })
+  }
+  /* eslint-disable */
+  listItemClick() {
+
+  }
+  /* eslint-enable */
 
   render() {
+    const { tabIndex } = this.state
+    // 测试假数据
+    const topic = {
+      title: 'This is title',
+      username: 'Jokcy',
+      reply_count: 20,
+      visit_count: 30,
+      create_at: 'afdfjdkfdjfjd',
+      tab: 'share',
+    }
     return (
       <Container>
         <Helmet>
           <title>This is topic list</title>
           <meta name="description" content="This is description" />
         </Helmet>
+        <Tabs value={tabIndex} onChange={this.changeTab}>
+          <Tab label="全部" />
+          <Tab label="分享" />
+          <Tab label="工作" />
+          <Tab label="问答" />
+          <Tab label="精品" />
+          <Tab label="测试" />
+        </Tabs>
+        <TopicListItem onClick={this.listItemClick} topic={topic} />
         <Button variant="raised" color="primary">This is a button</Button>
         <input type="text" onChange={this.changeName} />
         <span>{this.props.appState.msg}</span>
---
+++ client/views/topic-list/list-item.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import ListItem from 'material-ui/List/ListItem'
+import ListItemAvatar from 'material-ui/List/ListItemAvatar'
+import ListItemText from 'material-ui/List/ListItemText'
+import { withStyles } from 'material-ui/styles'
+// import Avatar from 'material-ui/Avatar'
+
+import IconHome from '@material-ui/icons/Home'
+// 引入自定义的样式文件
+import { topicPrimaryStyle, topicSecondaryStyle } from './styles'
+// 组件中使用样式，要传入classes
+const Primary = ({ classes, topic }) => (
+  <div className={classes.root}>
+    <span className={classes.tab}>{topic.tab}</span>
+    <span className={classes.title}>{topic.title}</span>
+  </div>
+)
+
+const Secondary = ({ classes, topic }) => (
+  <div className={classes.root}>
+    <span className={classes.userName}>{topic.username}</span>
+    <span className={classes.count}>
+      <span className={classes.accentColor}>{topic.reply_count}</span>
+      <span>/</span>
+      <span>{topic.visit_count}</span>
+    </span>
+    <span>创建时间：{topic.create_at}</span>
+  </div>
+)
+
+Primary.propTypes = {
+  topic: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+}
+Secondary.propTypes = {
+  topic: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired,
+}
+// withStyles作为装饰器，必须用在class上，不能用在pure function上
+// 所以这里要新定义一个组件（类）,后面在使用时替换原始组件定义Primary和Secondary
+const StyledPrimary = withStyles(topicPrimaryStyle)(Primary)
+const StyleSecondary = withStyles(topicSecondaryStyle)(Secondary)
+// 默认组件。topic表示整个话题topic对象
+const TopicListItem = ({ onClick, topic }) => (
+  <ListItem button onClick={onClick}>
+    <ListItemAvatar>
+      {/* <Avatar src={topic.image} /> */}
+      <IconHome />
+    </ListItemAvatar>
+    <ListItemText
+      primary={<StyledPrimary topic={topic} />}
+      secondary={<StyleSecondary topic={topic} />}
+    />
+  </ListItem>
+)
+
+TopicListItem.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  topic: PropTypes.object.isRequired,
+}
+
+export default TopicListItem
+
