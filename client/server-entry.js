83ba7ed9c2263aabfd6e70686adc14744b315902
bwyfed <36172659@qq.com>
2018-05-09 14:13:13 +0800

    服务端渲染时打包优化，解决mobx报错问题，增加SEO功能。

--- client/server-entry.js
+++ client/server-entry.js
@@ -7,7 +7,7 @@ import { Provider, useStaticRendering } from 'mobx-react'
 import App from './views/App'
 
 import { createStoreMap } from './store/store'
-// 使用静态渲染。让mobx在服务端渲染的时候不会重复数据变换
+// 使用静态渲染。让mobx在服务端渲染的时候不会重复数据变换。
 useStaticRendering(true);
 export default (stores, routerContext, url) => (
   <Provider {...stores}>
